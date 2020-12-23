fe45d902c76eb6a8c9ba7e1b46d7010850e35ada
Sara Wilson <sarakatewilson98@gmail.com>
2020-12-23 16:56:48 -0600

    updating javascript local storage

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,48 @@
 // Get an API Key from OpenWeatherMap API
-var apiKey ="1ea2723652fb48157c3662902e2f935d"
+var apiKey = '0b569f610d7ec3bee422760c10d3c145'
+
+// var queryUrl_Cord= 'https://api.openweathermap.org/data/2.5/weather?units=imperial&appid=1ea2723652fb48157c3662902e2f935d'
 
 
-function citySearch(cityName){
-    var queryURL = 'api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid'  + apiKey + '&units=imperial'
-    $.ajax({
-        url: queryURL,
-        method: 'GET',
-      }).then(function(weatherData){
-          console.log(weatherData)
-        // $('.city').text("City: " + weatherData.name)
-        // $('.wind').text("wind speed: " + weatherData.wind.speed)
-        // $('.humidity').text("humidity: " + weatherData.main.humidity)
-        // $('.temp').text("temperature: " + weatherData.main.temp)
-      })
+function citySearch(cityName) {
+  var queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid' + apiKey
+  $.ajax({
+    url: queryURL,
+    method: 'GET',
+  }).then(function (weatherData) {
+    console.log(weatherData)
+    // $('.city').text("City: " + weatherData.name)
+    // $('.wind').text("wind speed: " + weatherData.wind.speed)
+    // $('.humidity').text("humidity: " + weatherData.main.humidity)
+    // $('.temp').text("temperature: " + weatherData.main.temp)
+  })
 }
 
- $('#search-button').click(function(){
-    citySearch($('#city-input').val())
- })
+$('#search-button').click(function () {
+  citySearch($('#city-input').val())
+  // Search city when search button is clicked 
+
+  // local storage & history
+  var cityAr= localStorage.getItem('history')
+  if(cityAr === null){
+    cityAr=[]
+  }
+  cityAr.push($('#city-input').val())
+  localStorage.setItem('history', cityAr)
+  
+
+//   function search(event) {
+//     event.preventDefault();
+//     cityInput = $('#search-input').val().trim();
+//     if (cityInput !== '') {
+//       searchCity = cityInput;
+//       getWeather(searchCity);
+    // }
+//   }
+})
+
+//match array with buttons on html
+// get item from local storage and loop through
 
 
 
@@ -36,10 +60,21 @@ function citySearch(cityName){
 
 // WHEN I view future weather conditions for that city
 // THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
+function buildCards(){
+  for (i=0; i<5; i++){
+    var card= $('<div>').addClass('card')
+    var header= $('<h1>')
+    header.text(i)
+    card.append(header)
+    $('#5day').append(card)
+  }
+}
+buildCards()
 
 
 // WHEN I click on a city in the search history
 // THEN I am again presented with current and future conditions for that city-- go through local storage and make button for each previously searched city- button on click function- grab value from the button
+
 
 
 // WHEN I open the weather dashboard
